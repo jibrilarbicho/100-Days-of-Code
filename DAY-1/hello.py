3cd40d60d3e0c2eda282128fe847b456b8b3cd82
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-10-19 20:10:02 +0300

    100 Days Python Complete Bootcamp

---
+++ DAY-1/hello.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print("print('what to print')")
+print("HEllo world \n hello world")
+print("Hello" + " "+"Jibril")
+
+
+# python input function
+
+# print("Hello" + input("what is your name"))
+print(len("jimma"))
+
+#Python Variables
+print(len("jimma Abba jifar"))
+jimma=len("jimma Abba jifar")
+print(jimma)
