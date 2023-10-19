94c852eb95ec8edec2b891160fdece52aaff8b9c
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-10-19 21:33:21 +0300

    Tip  calculator

--- DAY-2/main.py
+++ DAY-2/main.py
@@ -24,3 +24,15 @@ days_remaining=years_reamaining*365
 weeks_remaining=years_reamaining*52
 months_remainig=years_reamaining*12
 print(f"you have {days_remaining} days,{weeks_remaining} weeks,{months_remainig} months left")
+# Tip Calculator
+print("Wlecome to tip calcultor")
+total_bill = float(input("What was the total bill $"))
+tip_percentage = int(input("How much would you like to give as a tip? 10, 12, or 15"))
+people = int(input("How many people to split the bill?"))
+tip_amount = total_bill * (tip_percentage / 100)
+bill_with_tip = total_bill + tip_amount
+
+amount_per_person = bill_with_tip / people
+final_amount = round(amount_per_person, 2)
+
+print(f"Each person should pay ${final_amount:.2f}")
