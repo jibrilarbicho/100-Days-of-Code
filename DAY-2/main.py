338c6e86b3594833c538fd0bd2c9dc957408c4b5
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-10-19 20:54:54 +0300

    Number manipulation and f string

--- DAY-2/main.py
+++ DAY-2/main.py
@@ -9,3 +9,11 @@ print("Your name has " +numchar +" Characters")
 print(100 +float("100.73"))
 print(str(100)+str(100))
 print("hello"[3])
+# Number manipulation and F string
+print(int(8/3))
+print(round(8/3,3))
+# f String 
+score=10
+height=176
+wining=True
+print(f"your score is {score},your height is {height}, You are winning is {wining}")
