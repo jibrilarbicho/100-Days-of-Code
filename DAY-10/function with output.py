8df51d5ef92f0a1f2b300cc047abacfa78c0b003
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-07 18:21:43 +0300

    Multiple return values

--- DAY-10/function with output.py
+++ DAY-10/function with output.py
@@ -2,10 +2,12 @@ from traceback import print_tb
 
 
 def formatName(fname, lname):
+    if fname == "" or lname == "":
+        return "You did not enter First Name or Last Name"
     f_name = fname.title()
     l_name = lname.title()
     return f"{f_name} {l_name}"
 
 
-Name = formatName("jibril", "arbicho")
+Name = formatName(input("What is your first Name\n"), input("what is your Last name\n"))
 print(Name)
