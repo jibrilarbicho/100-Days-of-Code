4f7d8929c95b6e73773fe1e5ea67ff4a3820cc62
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-07 20:00:30 +0300

    While loop

--- DAY-10/Calculator.py
+++ DAY-10/Calculator.py
@@ -15,15 +15,34 @@ def div(a, b):
 
 
 operations = {"+": add, "-": sub, "*": mult, "/": div}
-num1 = int(input("Enter first number\n"))
-num2 = int(input("Enter second number\n"))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick Operation from the above line:")
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
-print(f"{num1} {operation_symbol} {num2}={answer}")
-num3 = int(input("what is the next nubmer?: "))
-calculation_function = operations[operation_symbol]
-secanswer = calculation_function(answer, num3)
-print(f"{answer} {operation_symbol} {num3}={secanswer}")
+
+
+def caclulator():
+    num1 = int(input("Enter first number\n"))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick Operation from the above line:")
+        num2 = int(input("Enter next number\n"))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2}={answer}")
+        # num3 = int(input("what is the next nubmer?: "))
+        # calculation_function = operations[operation_symbol]
+        # secanswer = calculation_function(answer, num3)
+        # print(f"{answer} {operation_symbol} {num3}={secanswer}")
+        if (
+            input(
+                "type 'y' to continue calualting with {answer}, or type 'x' to exit: "
+            )
+            == "y"
+        ):
+            num1 = answer
+        else:
+            should_continue = False
+            caclulator()
+
+
+caclulator()
