af112d69b56aad91b45c12b0c492ab0317f44cfe
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-22 21:09:34 +0300

    Solution of Guessing Number Game

---
+++ DAY-12/guessingNumber.py
@@ -0,0 +1,47 @@
+from random import randint
+
+EASSY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+turns = 0
+
+
+def check_answer(guess, answer, turns):
+    if guess > answer:
+        print("To high")
+        return turns - 1
+    elif guess < answer:
+        print("To less")
+        return turns - 1
+
+    else:
+        print(f"You got it the answer was {answer}")
+
+
+def set_difficulty():
+    level = input("Choose a difficulty Type 'easy' or 'High'")
+    if level == "eassy":
+        return HARD_LEVEL_TURNS
+    else:
+        return EASSY_LEVEL_TURNS
+
+
+def game():
+    print("Welcome to the Guessing number Game")
+    print("I'm thinking a number between 1 and 100")
+    answer = randint(1, 100)
+    print(f"the correct answer is {answer}")
+    guess = 0
+    turns = set_difficulty()
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number")
+
+        guess = int(input("make a guess"))
+
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You Lost")
+            return
+
+
+game()
---
+++ DAY-12/main.py
@@ -0,0 +1,10 @@
+en = 12
+
+
+def en():
+    en = 1
+    print(f"enemies inside function:{en}")
+
+
+en()
+print(f"enemies outside funtion:{en}")
