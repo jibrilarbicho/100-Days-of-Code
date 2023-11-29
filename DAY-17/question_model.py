13d65e20805afcbef11ce46897fdd0bcf59877f2
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-29 11:51:32 +0300

    creating Question class

---
+++ DAY-17/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+new_q = Question("what is the capital City of Ethiopia", "Finfinne")
+print(new_q.text)
