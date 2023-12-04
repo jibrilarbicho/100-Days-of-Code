963fd66fd7b3cbdf114f3bdab056b8de523625f0
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-04 23:26:16 +0300

    Drwawing Different shapes

--- DAY-18/main.py
+++ DAY-18/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import random
 
 thimy = Turtle()
 # thimy.shape("turtle")
@@ -7,11 +8,38 @@ thimy.color("red")
 #     thimy.forward(100)
 #     thimy.left(90)
 
-for _ in range(15):
-    thimy.forward(10)
-    thimy.penup()
-    thimy.forward(10)
-    thimy.pendown()
+# for _ in range(15):
+#     thimy.forward(10)
+#     thimy.penup()
+#     thimy.forward(10)
+#     thimy.pendown()
 
-screen = Screen()
-# screen.exitonclick()
+# screen = Screen()
+# # screen.exitonclick()
+
+colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "pink",
+    "brown",
+    "black",
+    "white",
+    "gray",
+    "cyan",
+]
+
+
+def draw_shape(num_sides):
+    for _ in range(num_sides):
+        angle = 360 / num_sides
+        thimy.forward(100)
+        thimy.right(angle)
+
+
+for shape_side in range(3, 12):
+    thimy.color(random.choice(colors))
+    draw_shape(shape_side)
