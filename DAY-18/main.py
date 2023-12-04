978339162a88174ba01933063fc146d56351a384
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-04 22:55:57 +0300

    drawing dashed by turtle

--- DAY-18/main.py
+++ DAY-18/main.py
@@ -1,12 +1,17 @@
 from turtle import Screen, Turtle
 
 thimy = Turtle()
-thimy.shape("turtle")
+# thimy.shape("turtle")
 thimy.color("red")
-for _ in range(4):
-    thimy.forward(100)
-    thimy.left(90)
+# for _ in range(4):
+#     thimy.forward(100)
+#     thimy.left(90)
 
+for _ in range(15):
+    thimy.forward(10)
+    thimy.penup()
+    thimy.forward(10)
+    thimy.pendown()
 
 screen = Screen()
 # screen.exitonclick()
