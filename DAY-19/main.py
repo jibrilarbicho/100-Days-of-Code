12f4e1e27844f32eb7a15c54355988c7ad2d505f
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-06 22:16:56 +0300

    Understanding The Turtle Coordinate System

--- DAY-19/main.py
+++ DAY-19/main.py
@@ -2,41 +2,53 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
-screen.listen()
+# screen.listen()
 
 
-def move_forwards():
-    tim.forward(10)
+# def move_forwards():
+#     tim.forward(10)
 
 
-def move_backwards():
-    tim.backward(10)
+# def move_backwards():
+#     tim.backward(10)
 
 
-def turn_left():
-    new_heading = tim.heading() + 10
-    tim.setheading(new_heading)
+# def turn_left():
+#     new_heading = tim.heading() + 10
+#     tim.setheading(new_heading)
 
 
-def turn_right():
-    new_heading = tim.heading() - 10
-    tim.setheading(new_heading)
+# def turn_right():
+#     new_heading = tim.heading() - 10
+#     tim.setheading(new_heading)
 
 
-def clear():
-    tim.clear()
-    tim.home()
+# def clear():
+#     tim.clear()
+#     tim.home()
 
 
-screen.listen()
-screen.onkey(move_forwards, "w")
-screen.onkey(move_backwards, "s")
-screen.onkey(turn_left, "a")
-screen.onkey(turn_right, "d")
-screen.onkey(clear, "c")
-
-screen.exitonclick()
-
-
+# screen.listen()
+# screen.onkey(move_forwards, "w")
+# screen.onkey(move_backwards, "s")
+# screen.onkey(turn_left, "a")
+# screen.onkey(turn_right, "d")
+# screen.onkey(clear, "c")
 # timy = Turtle()
 # tomy = Turtle()
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(
+    title="Make Your Bet", prompt="Which Turtle will win the race ? Enter a Color"
+)
+y_positions = [-70, -40, -10, 20, 50, 80]
+print(user_bet)
+for turtle_index in range(0, 6):
+    tim = Turtle()
+    tim.penup()
+
+    tim.goto(x=-230, y=y_positions[turtle_index])
+    tim.shape("turtle")
+
+
+screen.exitonclick()
