38e5827aaf741e76091789ed908a5d38d7ea3f4a
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-09 14:54:36 +0300

    Animating snake segments on screen

--- DAY-20/main.py
+++ DAY-20/main.py
@@ -13,9 +13,10 @@ starting_position = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
 for position in starting_position:
     segment1 = Turtle("square")
-    segment1.penup()
 
     segment1.color("white")
+    segment1.penup()
+
     segment1.goto(position)
     segments.append(segment1)
 gameIsON = TRUE
@@ -23,6 +24,10 @@ while gameIsON:
     screnn.update()
     time.sleep(0.6)
 
-    for seg in segments:
-        seg.forward(20)
+    for seg in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg - 1].xcor()
+        new_y = segments[seg - 1].ycor()
+        segments[seg].goto(new_x, new_y)
+    segments[0].forward(20)
+
 screnn.exitonclick()
