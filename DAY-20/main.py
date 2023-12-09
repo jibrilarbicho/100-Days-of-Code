40b62b8e4d63ac7664479daae34892da064f1acc
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-09 15:54:16 +0300

    Control the snake with keypress

--- DAY-20/main.py
+++ DAY-20/main.py
@@ -1,33 +1,27 @@
 from pickle import TRUE
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 import turtle
+
+snake = Snake()
 
 screnn = Screen()
 screnn.setup(width=600, height=600)
 screnn.bgcolor("black")
 screnn.title("My Snake Game")
 screnn.tracer(0)
+screnn.listen()
+screnn.onkey(snake.Up, "Up")
+screnn.onkey(snake.Down, "Down")
+screnn.onkey(snake.Left, "Left")
+screnn.onkey(snake.Right, "Right")
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in starting_position:
-    segment1 = Turtle("square")
-
-    segment1.color("white")
-    segment1.penup()
-
-    segment1.goto(position)
-    segments.append(segment1)
 gameIsON = TRUE
 while gameIsON:
     screnn.update()
-    time.sleep(0.6)
+    time.sleep(0.1)
+    snake.move()
 
-    for seg in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg - 1].xcor()
-        new_y = segments[seg - 1].ycor()
-        segments[seg].goto(new_x, new_y)
-    segments[0].forward(20)
 
 screnn.exitonclick()
---
+++ DAY-20/snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle, Screen
+
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+FORWAD_DISTANCE = 20
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            segment1 = Turtle("square")
+
+            segment1.color("white")
+            segment1.penup()
+
+            segment1.goto(position)
+            self.segments.append(segment1)
+
+    def move(self):
+        for seg in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(new_x, new_y)
+
+        self.head.forward(FORWAD_DISTANCE)
+
+    def Up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def Down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def Left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def Right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
