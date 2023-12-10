dfe9ad24054e9cf9f47dce13e891e6ae4e67d0cd
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-10 15:26:12 +0300

    Ball class and make Ball  to Move

--- DAY-22/main.py
+++ DAY-22/main.py
@@ -1,9 +1,30 @@
 from turtle import Turtle, Screen
-import turtle
 
-turtle = Turtle()
+
 scren = Screen()
-scren.setup(800, 800)
+scren.tracer(0)
+scren.setup(800, 600)
 scren.title("Pong Game")
 scren.bgcolor("black")
+from paddle import Paddle
+from ball import Ball
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+ball.move
+# paddle = Turtle()
+
+# p
+# paddle.goto(350, 0)
+scren.listen()
+
+
+Gameison = True
+while Gameison:
+    scren.update()
+    scren.onkey(r_paddle.goup, "Up")
+    scren.onkey(r_paddle.godown, "Down")
+    scren.onkey(l_paddle.goup, "w")
+    scren.onkey(l_paddle.godown, "s")
 scren.exitonclick()
