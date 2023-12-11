b43a9476b3301b9b5bed25c7d6f68b89edd73294
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-12 00:03:37 +0300

    US States Game Part 2 Challenge with csv

--- DAY-25/US_States_Game/main.py
+++ DAY-25/US_States_Game/main.py
@@ -6,6 +6,9 @@ screen.title("U.S States Game")
 screen.addshape(image)
 
 turtle.shape(image)
+import pandas
+
+guessed_states = []
 
 
 def get_mouse_click_coor(x, y):
@@ -13,5 +16,27 @@ def get_mouse_click_coor(x, y):
 
 
 turtle.onscreenclick(get_mouse_click_coor)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(
+        title=(f"{len(guessed_states)}/50 Staes Correct"),
+        prompt="What is another state Name?",
+    ).title()
+    print(answer_state)
+
+    data = pandas.read_csv(
+        "/home/jibril/Documents/Python/DAY-25/US_States_Game/50_states.csv"
+    )
+    StateList = data.state.tolist()
+    if answer_state in StateList:
+        t = turtle.Turtle()
+        guessed_states.append(answer_state)
+        t.hideturtle()
+        t.penup()
+        stateData = data[data.state == answer_state]
+        t.goto(int(stateData.x), int(stateData.y))
+        # t.write(answer_state)
+        t.write(stateData.state.item())
+
+
 turtle.mainloop()
 # screen.exitonclick()
