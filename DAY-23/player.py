c103a133f54a5bb6cea6d28dc4c7460aadf0eae3
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-11 13:54:13 +0300

    Creating The Player Behaviour

---
+++ DAY-23/player.py
@@ -0,0 +1,17 @@
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+from turtle import Turtle
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def goup(self):
+        self.forward(MOVE_DISTANCE)
