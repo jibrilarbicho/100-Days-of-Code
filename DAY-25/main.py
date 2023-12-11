e860e1b2db2c952a65168f23038220375025d60d
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-11 20:15:12 +0300

    Reading CSV Files with the pandas library

--- DAY-25/main.py
+++ DAY-25/main.py
@@ -1,12 +1,16 @@
-import csv
+# import csv
 
 
-with open("/home/jibril/Documents/Python/DAY-25/weather-data.csv") as data_file:
-    data = csv.reader(data_file)
-    tempratures = []
-    for row in data:
-        if row[1] != "temp":
-            tempratures.append(int(row[1]))
-print(tempratures)
+# with open("/home/jibril/Documents/Python/DAY-25/weather-data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     tempratures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             tempratures.append(int(row[1]))
+# print(tempratures)
 
-# print(data)
+# # print(data)
+import pandas
+
+data = pandas.read_csv("/home/jibril/Documents/Python/DAY-25/weather-data.csv")
+print(data)
