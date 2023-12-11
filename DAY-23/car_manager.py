3018a570fd0ba4ff0e0124edab2135df20025095
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-11 14:48:53 +0300

    Creating the Car behaviour

--- DAY-23/car_manager.py
+++ DAY-23/car_manager.py
@@ -1,7 +1,28 @@
+import random
+import turtle
+
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+from turtle import Turtle
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.allCars = []
+
+    def create_car(self):
+        choice = random.randint(1, 6)
+        if choice == 1:
+            newCar = Turtle("square")
+            newCar.shapesize(stretch_wid=1, stretch_len=2)
+            newCar.penup()
+            newCar.color(random.choice(COLORS))
+            randomy = random.randint(-250, 250)
+            newCar.goto(300, randomy)
+            self.allCars.append(newCar)
+
+    def moveCar(self):
+        for car in self.allCars:
+            car.backward(STARTING_MOVE_DISTANCE)
--- DAY-23/main.py
+++ DAY-23/main.py
@@ -8,9 +8,12 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+car_manager = CarManager()
 screen.listen()
 screen.onkey(player.goup, "Up")
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.moveCar()
