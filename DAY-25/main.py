f37f54a030415b6f7aa69b0aa6d5851f998f2521
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-11 21:28:24 +0300

    Data Frames and Series Working With Rows and Columns

--- DAY-25/main.py
+++ DAY-25/main.py
@@ -13,4 +13,24 @@
 import pandas
 
 data = pandas.read_csv("/home/jibril/Documents/Python/DAY-25/weather-data.csv")
+# data_dict = data.to_dict()  # change it to dictionary
+# print(data_dict)
+# temp_list = data[
+#     "temp"
+# ].tolist()  # it takes one column which that are bellow temp column
+# print(temp_list)
+# print(data["temp"].max())
+# print(data["temp"].min())
+# print(data["temp"].mean())
+
+# print(data[data.day == "Monday"])  # it treated as dictionary
+# print(data)
+# print(data["temp"].max())  # the same as the bellow
+# print(data.temp.max())
+# print(data[data.temp == data.temp.max()])
+monday = data[data.day == "Monday"]
+print(monday.condition)
+# Create a dataframe from scratch
+data_dict = {"students": ["Amy", "James", "Angela"], "scores": [76, 56, 65]}
+data = pandas.DataFrame(data_dict)
 print(data)
