67a1886270548230913bd130fdddffee48e5610b
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-19 10:15:04 +0300

    Solution for NATO Alphabet Project

--- DAY-26/dcitionary Comprehension.py
+++ DAY-26/dcitionary Comprehension.py
@@ -7,3 +7,26 @@ passed_studnet = {
     student: score for (student, score) in students_score.items() if score >= 60
 }
 print(passed_studnet)
+sentence = "jimma shashemene asella asendabo sokkorru robe"
+
+word = {word: len(word) for word in sentence.split(" ")}
+print(word)
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {DAY: (TEMP * 9 / 5) + 32 for (DAY, TEMP) in weather_c.items()}
+print(weather_f)
+student_dict = {"student": ["Angella", "james", "jibril"], "score": [56, 76, 89]}
+import pandas
+
+student_DataFrames = pandas.DataFrame(student_dict)
+print(student_DataFrames)
+for index, row in student_DataFrames.iterrows():
+    print(row)
