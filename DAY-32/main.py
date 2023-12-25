620561a482438d15e5f5d2044f74013b7458b492
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-25 19:24:40 +0300

    Sending Emails using Python

---
+++ DAY-32/main.py
@@ -0,0 +1,9 @@
+import smtplib
+
+myEmail = "jibho2@gail.com"
+password = "errj4a4rt2#()"
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=myEmail, password=password)
+connection.sendmail(from_addr == myEmail, to_addrs="arbichoj@gmil.com", msg="Helllo")
+connection.close()
