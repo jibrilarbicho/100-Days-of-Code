365ae0ccec25c1e13d4f92fc2eeeb1036de4d3e5
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-01-01 14:30:01 +0300

    Working With Responses HTTP Codes Exceptions & JSON Data

--- DAY-32/main.py
+++ DAY-32/main.py
@@ -1,9 +1,14 @@
-import smtplib
+# import smtplib
 
-myEmail = "jibho2@gail.com"
-password = "errj4a4rt2#()"
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=myEmail, password=password)
-connection.sendmail(from_addr == myEmail, to_addrs="arbichoj@gmil.com", msg="Helllo")
-connection.close()
+# myEmail = "jibho2@gail.com"
+# password = "errj4a4rt2#()"
+# connection = smtplib.SMTP("smtp.gmail.com")
+# connection.starttls()
+# connection.login(user=myEmail, password=password)
+# connection.sendmail(from_addr == myEmail, to_addrs="arbichoj@gmil.com", msg="Helllo")
+# connection.close()
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+print(year)
--- DAY-33/main.py
+++ DAY-33/main.py
@@ -1,4 +1,13 @@
+from traceback import print_tb
 import requests
 
 res = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(res)
+# if res.status_code == 404:
+#     raise Exception("The Resource Does not found")
+# elif res.status_code == 401:
+#     raise Exception("You are not Authorized to access this data")
+res.raise_for_status()
+
+data = res.json()
+print(data)
+longitude = data["iss_position"]["longitude"]
