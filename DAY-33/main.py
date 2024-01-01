2df8043610148361aaa29c19a67ac9eabdc97b37
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-01-01 21:59:03 +0300

    300 - Understand API Parameters Match Sunset Times with the Current Time

--- DAY-33/main.py
+++ DAY-33/main.py
@@ -1,5 +1,6 @@
 # from traceback import print_tb
-# import requests
+import requests
+from datetime import datetime
 
 # res = requests.get(url="http://api.open-notify.org/iss-now.json")
 # # if res.status_code == 404:
@@ -11,39 +12,52 @@
 # data = res.json()
 # print(data)
 # longitude = data["iss_position"]["longitude"]
-from tkinter import *
-import requests
+# from tkinter import *
+# import requests
 
 
-def get_quote():
-    response = requests.get("https://api.kanye.rest")
-    response.raise_for_status()
-    result = response.json()
-    quote = result["quote"]
-    # Write your code here.
-    canvas.itemconfig(quote_text, quote)
+# def get_quote():
+#     response = requests.get("https://api.kanye.rest")
+#     response.raise_for_status()
+#     result = response.json()
+#     quote = result["quote"]
+#     # Write your code here.
+#     canvas.itemconfig(quote_text, quote)
 
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+# window = Tk()
+# window.title("Kanye Says...")
+# window.config(padx=50, pady=50)
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="/home/jibril/Documents/Python/DAY-33/background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(
-    150,
-    207,
-    text="Kanye Quote Goes HERE",
-    width=250,
-    font=("Arial", 30, "bold"),
-    fill="white",
-)
-canvas.grid(row=0, column=0)
+# canvas = Canvas(width=300, height=414)
+# background_img = PhotoImage(file="/home/jibril/Documents/Python/DAY-33/background.png")
+# canvas.create_image(150, 207, image=background_img)
+# quote_text = canvas.create_text(
+#     150,
+#     207,
+#     text="Kanye Quote Goes HERE",
+#     width=250,
+#     font=("Arial", 30, "bold"),
+#     fill="white",
+# )
+# canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file="/home/jibril/Documents/Python/DAY-33/kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+# kanye_img = PhotoImage(file="/home/jibril/Documents/Python/DAY-33/kanye.png")
+# kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+# kanye_button.grid(row=1, column=0)
 
 
-window.mainloop()
+# window.mainloop()
+
+MYLAT = 9.145000
+MYLONG = 40.489674
+paramters = {"lat": MYLAT, "lng": MYLONG, "formatted": 0}
+response = requests.get("https://api.sunrise-sunset.org/json", params=paramters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+
+sunset = data["results"]["sunset"]
+
+print(sunrise.split("T")[1].split(":"), sunset)
+timeNow = datetime.now()
