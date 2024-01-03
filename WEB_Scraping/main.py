4fd1fb40c7014c0713a222774823fddbf5b10edb
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-01-03 19:50:02 +0300

    Scraping Live websites

--- WEB_Scraping/main.py
+++ WEB_Scraping/main.py
@@ -1,13 +1,15 @@
 from bs4 import BeautifulSoup
-from certifi import contents
+import requests
 
-with open("/home/jibril/Documents/Python/WEB_Scraping/website.html") as file:
-    contents = file.read()
-soup = BeautifulSoup(contents, "html.parser")
-all_anchor_tag = soup.findAll(name="a")
-for anch in all_anchor_tag:
-    print(anch.getText())
-heading = soup.find(name="h1", id="name")
-print(heading)
-headings = soup.select(".heading")
-print("headings", headings)
+# with open("/home/jibril/Documents/Python/WEB_Scraping/website.html") as file:
+#     contents = file.read()
+# soup = BeautifulSoup(contents, "html.parser")
+# all_anchor_tag = soup.findAll(name="a")
+# for anch in all_anchor_tag:
+#     print(anch.getText())
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+# headings = soup.select(".heading")
+# print("headings", headings)
+response = requests.get("https://news.ycombinator.com/news")
+print(response.text)
